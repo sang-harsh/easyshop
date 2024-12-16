e8feeccc8eec987d4e4015fd6477f37bc46ab350
Khandagale (EXT), Sangharsh kvbj6y <sangharsh.khandagale.ext@siemens.com>
2024-12-16 15:31:22 +0530

    removed server folder

--- server/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    useJSON: true, // Toggle between true (JSON files) and false (MongoDB)
-    mongoURI: 'mongodb://localhost:27017/easyshop', // MongoDB connection string , need to replace with correct string . ALso need to hide it.
-    jsonFilePath: './data/items.json', // Path to the JSON file. Not sure if we will need it here
-  };
-  
-  module.exports = config;
-  
--- server/models/Item.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const itemSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  quantity: { type: Number, default: 1 },
-  description: { type: String }
-});
-
-module.exports = mongoose.model('Item', itemSchema);
